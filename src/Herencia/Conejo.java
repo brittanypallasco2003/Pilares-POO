701cab0ea3f03a74485eb56c9cb3f39c70a28af8
brittanypallasco2003 <nohemiespinel0@gmail.com>
2023-08-20 18:16:56 -0500

    clases hijas conejo y perro

--- src/Herencia/Conejo.java
+++ src/Herencia/Conejo.java
@@ -1,4 +1,27 @@
 package Herencia;
+public class Conejo extends Mascota{
+    private String razaConejo;
+    private float pesoKg;
 
-public class Conejo {
+    public Conejo(String nomMascota,
+                  String nomProp,
+                  String dieta,
+                  String razaConejo,
+                  float pesoKg) {
+        super(nomMascota, nomProp, dieta);
+        this.razaConejo=razaConejo;
+        this.pesoKg=pesoKg;
+    }
+    public String getRazaConejo() {
+        return razaConejo;
+    }
+    public void setRazaConejo(String razaConejo) {
+        this.razaConejo = razaConejo;
+    }
+    public float getPesoKg() {
+        return pesoKg;
+    }
+    public void setPesoKg(float pesoKg) {
+        this.pesoKg = pesoKg;
+    }
 }
